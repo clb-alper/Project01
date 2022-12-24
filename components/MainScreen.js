ada8937ba985b25fdd2bf2d686455dfab133bb99
Frizy <frizyhyperfusionfall@gmail.com>
2022-12-24 13:37:29 +0300

    MainScreen Updated
    Header Added

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,76 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useCallback } from 'react';
-import { StyleSheet, Text, View, TextInput, Image, Pressable, TouchableHighlight, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Pressable, TouchableHighlight, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import colors from '../assets/colors/colors';
+import { SafeAreaView } from 'react-native-safe-area-context';
 const MainScreen = ({ navigation }) => {
 
-    var [isPress, setIsPress] = React.useState(false);
-  
-    const [fontsLoaded] = useFonts({
-      'Comic-Regular': require('../assets/fonts/ComicNeue-Regular.ttf'),
-      'Comic-Light': require('../assets/fonts/ComicNeue-Light.ttf'),
-      'Comic-Bold': require('../assets/fonts/ComicNeue-Bold.ttf'),
-    });
-  
-    const onLayoutRootView = useCallback(async () => {
-      if (fontsLoaded) {
-        await SplashScreen.hideAsync();
-      }
-    }, [fontsLoaded]);
-  
-    if (!fontsLoaded) {
-      return null;
+  var [isPress, setIsPress] = React.useState(false);
+
+  const [fontsLoaded] = useFonts({
+    'Comic-Regular': require('../assets/fonts/ComicNeue-Regular.ttf'),
+    'Comic-Light': require('../assets/fonts/ComicNeue-Light.ttf'),
+    'Comic-Bold': require('../assets/fonts/ComicNeue-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
-  
-    var touchPropsLoginButton = {
-      activeOpacity: 1,
-      underlayColor: '#ffe0e7',
-      style: isPress ? styles.loginButtonPressed : styles.loginButton,
-      onHideUnderlay: () => setIsPress(false),
-      onShowUnderlay: () => setIsPress(true),
-      onPress: () => navigation.navigate('Login')
-  
-    };
-    return (
-      <View style={styles.container} onLayout={onLayoutRootView}>
-        <StatusBar style="auto" />
-        <Text>MainScreen</Text>
-  
-        <TouchableHighlight {...touchPropsLoginButton} style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>{"Giriş Yap"}</Text>
-        </TouchableHighlight>
-      </View>
-    )
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
   }
-  
-  export default MainScreen
-  
-  const styles = StyleSheet.create({
-  
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: colors.yellowLight
-    },
-  
-    loginButton: {
-      alignItems: 'center',
-      width: '85%',
-      padding: 12,
-      backgroundColor: colors.pinkRegular,
-      borderWidth: 2,
-      borderRadius: 15,
-      borderColor: colors.pinkBorder
-    },
-  
-    loginButtonText: {
-      fontFamily: 'Comic-Light',
-      textAlign: 'center',
-      fontSize: 23,
-    },
-  
-  })
+
+  var touchPropsLoginButton = {
+    activeOpacity: 1,
+    underlayColor: '#ffe0e7',
+    style: isPress ? styles.loginButtonPressed : styles.loginButton,
+    onHideUnderlay: () => setIsPress(false),
+    onShowUnderlay: () => setIsPress(true),
+    onPress: () => navigation.navigate('Login')
+
+  };
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <StatusBar style="auto" />
+      <ScrollView>
+        <SafeAreaView>
+          <View style={styles.headerView1}>
+            
+            <Image source={require('../assets/images/iconBook.png')} style={styles.headerIconStyle}></Image>
+            <Text style={styles.headerTextStyle}>Hoşgeldin Ömer</Text>
+
+            <View style={styles.headerView2}>
+              <Text>"Sasd"</Text>
+            </View>
+
+          </View>
+          <TouchableHighlight {...touchPropsLoginButton} style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>{"Giriş Yap"}</Text>
+          </TouchableHighlight>
+        </SafeAreaView>
+      </ScrollView>
+    </View>
+  )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: colors.yellowLight
+  },
+
+  loginButton: {
+    alignItems: 'center',
+    width: '85%',
+    padding: 12,
+    backgroundColor: colors.pinkRegular,
+    borderWidth: 2,
+    borderRadius: 15,
+    borderColor: colors.pinkBorder
+  },
+
+  loginButtonText: {
+    fontFamily: 'Comic-Light',
+    textAlign: 'center',
+    fontSize: 23,
+  },
+
+  headerView1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 15,
+  },
+
+  headerView2: {
+    position: 'absolute',
+    right: '-4.5%',
+    marginHorizontal: 20,
+    marginTop: 15,
+  },
+
+  headerIconStyle: {
+    resizeMode: 'contain',
+    height: 35,
+    width: 35,
+  },
+
+  headerTextStyle: {
+    fontFamily: 'Comic-Regular',
+    marginLeft: '2.5%',
+    fontSize: 25,
+  },
+
+})
