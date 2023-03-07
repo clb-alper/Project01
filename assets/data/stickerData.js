81260cf171143ff3c91ba921b7af12d7b1f2c8ae
Frizy <frizyhyperfusionfall@gmail.com>
2023-03-07 15:09:19 +0300

    stickerData Added
    stickerData Added

---
+++ assets/data/stickerData.js
@@ -0,0 +1,122 @@
+
+const stickerData = [
+    {
+        id: '1',
+        stickerName: 'CatCake',
+        spPrice: 1600,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '2',
+        userName: 'CatCake',
+        spPrice: 1500,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '3',
+        userName: 'CatCake',
+        spPrice: 1200,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '4',
+        userName: 'CatCake',
+        spPrice: 1800,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '5',
+        userName: 'CatCake',
+        spPrice: 1700,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '6',
+        userName: 'CatCake',
+        spPrice: 1200,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '7',
+        userName: 'CatCake',
+        spPrice: 1500,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '8',
+        userName: 'CatCake',
+        spPrice: 1100,
+        stickerBook: "Book1",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '9',
+        userName: 'CatCake',
+        spPrice: 1000,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '10',
+        userName: 'CatCake',
+        spPrice: 1900,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '11',
+        userName: 'CatCake',
+        spPrice: 1500,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '12',
+        userName: 'CatCake',
+        spPrice: 1550,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '13',
+        userName: 'CatCake',
+        spPrice: 1450,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '14',
+        userName: 'CatCake',
+        spPrice: 1050,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '15',
+        userName: 'CatCake',
+        spPrice: 1250,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+    {
+        id: '16',
+        userName: 'CatCake',
+        spPrice: 1000,
+        stickerBook: "Book2",
+        image: require('../images/sticker1.png'),
+    },
+]
+
+export default stickerData;
+    
+
+
+
+
