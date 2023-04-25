7defa65818fc5974821fb5b26fa71b39d6d3364a
Frizy <frizyhyperfusionfall@gmail.com>
2023-04-25 15:02:45 +0300

    Translation Modal Updated
    Translation Modal Updated
    
    Co-Authored-By: gleRKN <92293685+gleRKN@users.noreply.github.com>
    Co-Authored-By: tarikhuvaz <79941324+tarikhuvaz@users.noreply.github.com>

--- assets/components/TranslationModal.js
+++ assets/components/TranslationModal.js
@@ -37,16 +37,33 @@ const TranslationModal = () => {
 
             <View style={styles.modalViewStyle}>
 
-                <TouchableOpacity
-                    onPress={() => setTranslationModalVisible(!translationModalVisible)}
-                    activeOpacity={0.75}>
-                    <IonIcons name="ios-close" size={40} color="#000" style={styles.modalBookDetailHeaderClose} />
-                </TouchableOpacity>
+                <View style={styles.modalBookDetailHeader}>
+                    <View>
+
+                    </View>
+                    <TouchableOpacity
+                        onPress={() => setTranslationModalVisible(!translationModalVisible)}
+                        activeOpacity={0.75}>
+                        <IonIcons name="ios-close" size={40} color="#000" style={styles.modalBookDetailHeaderClose} />
+                    </TouchableOpacity>
+                </View>
 
                 <View style={styles.dashboardContainer}>
-                    <Text> {typeof(translationModalEntry.trTranslation) != 'undefined' ? (translationModalEntry.trTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).charAt(0).toUpperCase() + (translationModalEntry.trTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).slice(1) : ""} </Text>
-                    <Text> = </Text>
-                    <Text> {typeof(translationModalEntry.engTranslation) != 'undefined' ? (translationModalEntry.engTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).charAt(0).toUpperCase() + (translationModalEntry.engTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).slice(1) : ""} </Text>
+                    <View>
+                        <Text style={styles.mainText2}> Türkçe </Text>
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            style={styles.mainText}> {typeof (translationModalEntry.trTranslation) != 'undefined' ? (translationModalEntry.trTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).charAt(0).toUpperCase() + (translationModalEntry.trTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).slice(1) : ""} </Text>
+                    </View>
+                    <Text style={styles.mainText}> = </Text>
+                    <View>
+                        <Text style={styles.mainText2}> İngilizce </Text>
+                        <Text
+                            adjustsFontSizeToFit
+                            numberOfLines={1}
+                            style={styles.mainText}> {typeof (translationModalEntry.engTranslation) != 'undefined' ? (translationModalEntry.engTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).charAt(0).toUpperCase() + (translationModalEntry.engTranslation.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).slice(1) : ""} </Text>
+                    </View>
                 </View>
 
             </View>
@@ -72,12 +89,37 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "row",
+        marginTop: -30,
+    },
+
+    translationNameContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: -20,
+        marginBottom: 20
     },
 
     modalBookDetailHeaderClose: {
         resizeMode: 'contain',
-        left: 25,
-        top: -2,
+    },
+
+    mainText: {
+        fontFamily: 'Comic-Regular',
+        textAlign: 'center',
+    },
+
+    mainText2: {
+        fontFamily: 'Comic-Regular',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+
+    modalBookDetailHeader: {
+        width: '92%',
+        top: -25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
 
 
