7375ca57a64b35271cf86668700ee531c0ac2432
gleRKN <92293685+gleRKN@users.noreply.github.com>
2023-05-08 12:50:24 +0300

    Translator.js updated
    Now we are using Google api for translation.
    
    Co-Authored-By: Yusuf <69965704+Frizy0@users.noreply.github.com>
    Co-Authored-By: clb-alper <61182961+clb-alper@users.noreply.github.com>

--- assets/components/Translator.js
+++ assets/components/Translator.js
@@ -1,27 +1,48 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+
+// const Translator = async (word) => {
+
+//     const url = 'https://api.mymemory.translated.net/get';
+
+//       const response = await axios.get(url, {
+//         params: {
+//           q: word,
+//           langpair: `tr|en`,
+//         },
+//       });
+
+//       console.log(word, ' -> ', response.data.responseData.translatedText)
+//       if (response.data && response.data.responseData && response.data.responseData.translatedText) {
+//         const translatedText = response.data.responseData.translatedText;
+//         // show on modal
+//         return translatedText;
+//       } else {
+//         throw new Error('Translation API response format is not as expected.');
+//       }
+
+// }
+
+// export {Translator}
+
+import { Translate } from '@google-cloud/translate';
+
+
+const projectId = "static-beach-383414"
+
+const translate = new Translate({ projectId });
+
 
 
 const Translator = async (word) => {
+    const target = 'tr';
+    const [translation] = await translate.translate(word, target);
+    console.log(`Text: ${word}`);
+    console.log(`Translation: ${translation}`);
 
-    const url = 'https://api.mymemory.translated.net/get';
-
-      const response = await axios.get(url, {
-        params: {
-          q: word,
-          langpair: `tr|en`,
-        },
-      });
-
-      console.log(word, ' -> ', response.data.responseData.translatedText)
-      if (response.data && response.data.responseData && response.data.responseData.translatedText) {
-        const translatedText = response.data.responseData.translatedText;
-        // show on modal
-        return translatedText;
-      } else {
-        throw new Error('Translation API response format is not as expected.');
-      }
+    return translation
 
 }
 
-export {Translator}
+export { Translator }
 
