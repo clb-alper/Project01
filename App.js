c24773813684043af496505d30bfbb3d1b0ac64f
clb-alper <61182961+clb-alper@users.noreply.github.com>
2023-06-03 11:25:55 +0300

    Default Font Size

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ import QuizPage from './pages/QuizPage';
 import BackpackScreen from './pages/BackpackScreen';
 import PuzzlePage from './pages/PuzzlePage';
 import ProfileEdit from './pages/Settings/ProfileEdit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Tab = createBottomTabNavigator();
@@ -124,6 +125,15 @@ function HomeScreen() {
 export default function App() {
   const Stack = createStackNavigator();
 
+  React.useEffect(() => {
+
+    const setDefaultFontSize = async () => {
+      await AsyncStorage.setItem('@profileFontSize:key', '20');
+
+    }
+    setDefaultFontSize();
+
+  }, [])
   return (
     <ProfileProvider>
       <RewardsProvider>
